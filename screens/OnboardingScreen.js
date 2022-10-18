3a7660f7a532580692bd831b8516bab05260a3bd
Leven Barney <bar22043@byui.edu>
2022-10-18 15:01:01 -0600

    Imported TextInput

--- screens/OnboardingScreen.js
+++ screens/OnboardingScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, Button, StyleSheet, Image, TouchableOpacity } from 'react-native';
+import { View, Text, Button, StyleSheet, Image, TouchableOpacity, TextInput } from 'react-native';
 import Onboarding from 'react-native-onboarding-swiper';
 import { SafeAreaView } from 'react-navigation';
 
@@ -43,8 +43,12 @@ const OnboardingScreen = ({setFirstLaunch}) =>{
             {
               backgroundColor: '#fff',
               image: <Image   style={{height: '60%', width: '90%', resizeMode:'contain', marginBottom:-200}} source={require('../image/testing.png')} />,
-              title: <Text style={{fontWeight:'bold', textAlign:'center', color:'#A0CE4E', fontSize:19, margin:12,  marginTop: -70}}>Welcome to STEDI  Balance</Text>,
-              subtitle: 'STEDI Balance will now send a text with a code',
+              subtitle: (
+                <View>
+                <Text style={{fontWeight:'bold', textAlign:'center', color:'#A0CE4E', fontSize:19, margin:12,  marginTop: -70}}>We will not share your data. Please share your phone number so we can track your balance.</Text>
+                <TextInput placeholder='Phone Number'/>
+                </View>
+              ),
             },
             {
                 backgroundColor: '#fff',
